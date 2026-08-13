b84b5c18e0e32dd319b1f4c6a699984b4e286972
Carlos Cardoso <cvcardos5@gmail.com>
2026-08-13 16:42:44 -0300

    Refactor CI configuration and enhance testing setup with Testcontainers

--- api/src/test/java/io/github/carloscardoso05/api/ApiApplicationTests.java
+++ api/src/test/java/io/github/carloscardoso05/api/ApiApplicationTests.java
@@ -2,9 +2,18 @@ package io.github.carloscardoso05.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ApiApplicationTests {
+
+    @Container
+    @ServiceConnection
+    private static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:18-alpine");
 
     @Test
     void contextLoads() {
